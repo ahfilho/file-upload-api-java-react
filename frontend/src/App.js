6e1d7363b9e93f31f5c0a20bb6553800c690c9b6
dinho <dinho-16@hotmail.com>
2022-10-21 14:52:42 -0300

    Listagem de produtos utilizando react. Identação.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,29 @@
-import './App.css';
-import Formulario from './formulario/Formulario';
-import List from './listagem/List';
-import {useEffect, useState} from 'react';
-
+import "./App.css";
+import Formulario from "./formulario/Formulario";
+import List from "./listagem/List";
+import { useEffect, useState } from "react";
 
 function App() {
-
   // UseState
-const [btnCadastrar, setBtnCadastrar] = useState(true);
-const [ssds, setSsds] = useState([]);
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [ssds, setSsds] = useState([]);
 
-// UseEffect
-useEffect(()=>{
-fetch("http://localhost:9090/ssd/list")
-.then(retorno => retorno.json())
-.then(retorno_convertido => setSsds(retorno_convertido));
+  // UseEffect
+  useEffect(() => {
+    fetch("http://localhost:9090/ssd/list")
+      .then((retorno) => retorno.json())
+      .then((retorno_convertido) => setSsds(retorno_convertido));
+  }, []); // as chaves vão permitir fazer apenas uma requisição. Se não tiver, entra em loop.
 
-}, []); // as chaves vão permitir fazer apenas uma requisição. Se não tiver, entra em loop.
-
-// Retorno
+  // Retorno
   return (
     <div className="App">
-  <h3>Cadastro de produtos: SSD</h3> 
-  <p>{JSON.stringify(ssds)}</p>
+      <h3>Cadastro de produtos: SSD</h3>
+      <p>{JSON.stringify(ssds)}</p>
 
-  <Formulario botao={btnCadastrar}/>
-        <List vetor={ssds}/>
+      <Formulario botao={btnCadastrar} />
+      <List vetor={ssds} />
       {/* <h3>Produtos cadastrados</h3> */}
-
     </div>
   );
 }
--- frontend/src/formulario/Formulario.js
+++ frontend/src/formulario/Formulario.js
@@ -1,48 +1,63 @@
-import './Form.css';
+import "./Form.css";
 
-function Form(){
-  
-    //uma funçao que retorna o form
-return(
-   <div class="meuForm">
-   
-   <form>
-        <input type="button"class="btn btn-secondary" value="Início"/>
-    <input type="button"class="btn btn-dark" value="Pesquisar"/>
+function Form() {
+  //uma funçao que retorna o form
+  return (
+    <div class="meuForm">
+      <form>
+        <input type="button" class="btn btn-secondary" value="Início" />
+        <input type="button" class="btn btn-dark" value="Pesquisar" />
 
-    <div class="form-row">
-    <div class=" col-md-6 offset-md-3">
-      <input type="text" class="form-control" placeholder="Brand"/>
+        <div class="form-row">
+          <div class=" col-md-6 offset-md-3">
+            <input type="text" class="form-control" placeholder="Brand" />
+          </div>
+          <div class=" col-md-6 offset-md-3">
+            <input type="text" class="form-control" placeholder="Model" />
+          </div>
+          <div class=" col-md-6 offset-md-3">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Serial number"
+            />
+          </div>
+          <div class=" col-md-6 offset-md-3">
+            <input type="text" class="form-control" placeholder="Size" />
+          </div>
+          <div class=" col-md-6 offset-md-3">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Purchase price"
+            />
+          </div>
+          <div class=" col-md-6 offset-md-3">
+            <input type="text" class="form-control" placeholder="Sale value" />
+          </div>
+          <div class=" col-md-6 offset-md-3">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Purchate date"
+            />
+          </div>
+          <div class=" col-md-6 offset-md-3">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Arrival date"
+            />
+          </div>
+          <div class="botao1">
+            {" "}
+            <input type="submit" class="btn btn-success" value="Cadastrar" />
+            <input type="button" class="btn btn-primary" value="Listar" />
+          </div>
+        </div>
+      </form>
     </div>
-     <div class=" col-md-6 offset-md-3">
-            <input type="text" class="form-control" placeholder="Model"/>
-    </div>
-     <div class=" col-md-6 offset-md-3">
-         <input type="text" class="form-control" placeholder="Serial number"/>
-    </div>
-     <div class=" col-md-6 offset-md-3">
-       <input type="text" class="form-control" placeholder="Size"/>
-    </div>
-     <div class=" col-md-6 offset-md-3">
-         <input type="text" class="form-control" placeholder="Purchase price"/>
-    </div>
-     <div class=" col-md-6 offset-md-3">
-         <input type="text" class="form-control" placeholder="Sale value"/>
-    </div>
-     <div class=" col-md-6 offset-md-3">
-        <input type="text" class="form-control" placeholder="Purchate date"/>
-    </div>
-     <div class=" col-md-6 offset-md-3">
-         <input type="text" class="form-control" placeholder="Arrival date"/>
-    </div>
-     <div class="botao1"> <input type="submit" class="btn btn-success" value="Cadastrar"/>
-    <input type="button"class="btn btn-primary" value="Listar"/>
-    
-</div>
-    </div>
-    </form> 
-    </div>
-)
+  );
 }
 
 export default Form;
