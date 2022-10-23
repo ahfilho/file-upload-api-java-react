867c1fd780afbc920bf60aa6bd2b44c83ef6bc21
dinho <dinho-16@hotmail.com>
2022-10-23 09:56:33 -0300

    Captura dos inputs do formulário

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,23 @@ import List from "./listagem/List";
 import { useEffect, useState } from "react";
 
 function App() {
-  // UseState
+
+//objeto SSD
+const ssd = {
+  brand : '',
+  model :'',
+  serialNumber : '',
+  size : '',
+  purchasePrice :'',
+  purchaseDate : '',
+  saleValue : '',
+  arrivalDate :''
+ 
+}
+  // UseState = cria  um objeto de maneira dinâmica
   const [btnCadastrar, setBtnCadastrar] = useState(true);
   const [ssds, setSsds] = useState([]);
+  const [objetoSsd, setObjetoSsd] = useState(ssd);  
 
   // UseEffect
   useEffect(() => {
@@ -15,13 +29,24 @@ function App() {
       .then((retorno_convertido) => setSsds(retorno_convertido));
   }, []); // as chaves vão permitir fazer apenas uma requisição. Se não tiver, entra em loop.
 
+//pegando os dados do formulário
+
+
+const teste = (e) => {
+  console.log(e.target);
+  setObjetoSsd({...objetoSsd, [e.target.brand]:e.target.model}); //para editar o objeto
+}
+
+
   // Retorno
   return (
     <div className="App">
-      <h3>Cadastro de produtos: SSD</h3>
-      <p>{JSON.stringify(ssds)}</p>
+      <h3>Cadastro de produtos: </h3>
+      {/* <p>{JSON.stringify(ssds)}</p> */}
+      <p>{JSON.stringify(objetoSsd)}</p>
 
-      <Formulario botao={btnCadastrar} />
+      <Formulario botao={btnCadastrar} eventoTeste={teste} />
+
       <List vetor={ssds} />
       {/* <h3>Produtos cadastrados</h3> */}
     </div>
