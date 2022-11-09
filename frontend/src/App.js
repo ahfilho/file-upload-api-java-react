262b7dbf83039219a5317b7033eebcdac8943d4d
dinho <dinho-16@hotmail.com>
2022-11-09 12:41:22 -0300

    Listagem: OK Adicionar: em construção

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,81 +1,148 @@
 import "./App.css";
-import Formulario from "./formulario/Formulario";
 import List from "./listagem/List";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
 
-//objeto SSD
-const ssd = {
-  brand: '',
-  model : '',
-  serialNumber : '',
-  size : '',
-  purchasePrice :'',
-  purchaseDate : '',
-  saleValue : '',
-  arrivalDate :''
+
+return(
+
+  <div className="App">
+    <List/>
+    
+  </div>
+)
+
+
+
+
+
+
+
+
+
+
+}
+
+// //objeto SSD
+// const ssd = {
+//   brand: '',
+//   model : '',
+//   serialNumber : '',
+//   size : '',
+//   purchasePrice :'',
+//   purchaseDate : '',
+//   saleValue : '',
+//   arrivalDate :''
  
-}
-  // UseState = cria  um objeto de maneira dinâmica
-  const [btnCadastrar, setBtnCadastrar] = useState(true);
-  
-  const [ssds, setSsds] = useState([]);
-  const [objetoSsd, setObjetoSsd] = useState(ssd);  
+// }
+// const categoria = {
+//   producoCategoria : ''
+// }
 
-  // UseEffect
-  useEffect(() => {
-    fetch("http://localhost:9090/ssd/list")
-      .then((retorno) => retorno.json())
-      .then((retorno_convertido) => setSsds(retorno_convertido));
-  }, []); // as chaves vão permitir fazer apenas uma requisição. Se não tiver, entra em loop.
-
-//pegando os dados do formulário
-
-//setState
-const aoDiditar = (e) => {
-    setObjetoSsd({...ssds,[e.target.brand]:e.target.value});
-    console.log(e.target.value);
-  }
-  //para editar o objeto
+//   // UseState = cria  um objeto de maneira dinâmica
+//   const [btnCadastrar, setBtnCadastrar] = useState(true);
+//   const [ssds, setSsds] = useState([]);
+//   const [objetoSsd, setObjetoSsd] = useState(ssd);   
+//   const [category, setCategoria] = useState();
 
 
-const cadastrar = () => {
-  console.log("testee");
-  fetch("http://localhost:9090/ssd/new", {
-    method: "post",
-    body: JSON.stringify(objetoSsd),
-    headers: {
-      "Content-type": "application/json",
-      Accept: "application/json",
-    },
-  })
-    .then((retorno) => retorno.json())
-    .then((retorno_convertido) => {
-      if (retorno_convertido.mensagem !== undefined) {
-        alert(retorno_convertido.mensagem);
-      } else {
-        setSsds([...ssds, retorno_convertido]);
-        alert("Produto cadastrado com sucesso!");
-        // limparFormulario();
-      }
-    });
-};
+//   // UseEffect
+//   useEffect(() => {
+//     axios.get("http://localhost:9090/ssd/list")
+//       .then((retorno) => retorno.json())
+//       .then((retorno_convertido) => setSsds(retorno_convertido));
+//   }, []); // as chaves vão permitir fazer apenas uma requisição. Se não tiver, entra em loop.
+
+// //pegando os dados do formulário
+
+// //setState
+// const aoDiditar = (e) => {
+//     setObjetoSsd({ ...objetoSsd, [e.target.brand]:e.target.value });
+
+//     console.log(e.target);
+//   }
+ 
+//   // const eventoCadastrar = () => {
+//   //   //objeto SSD
+//   //   const ssd = {
+//   //     brand: "",
+//   //     model: "",
+//   //     serialNumber: "",
+//   //     size: "",
+//   //     purchasePrice: "",
+//   //     purchaseDate: "",
+//   //     saleValue: "",
+//   //     arrivalDate: "",
+//   //   };
+
+//   //   const category = {
+//   //     productCategory: ""
+//   //   }
+//   //   const urll = "http://localhost:9090/ssd/new";
+
+//   //   fetch(urll, {
+//   //     method: "POST",
+//   //     body: JSON.stringify(objetoSsd),
+//   //     headers: {
+//   //       "Content-type": "application/json",
+//   //       Accept: "application/json",
+//   //     },
+//   //   })
+//   //     .then((response) => response.json())
+//   //     .then((json) => {
+//   //       console.log("JOSOONNN", json);
+//   //       setSsds(json);
+//   //     })
+//   //     .catch((e) => {
+//   //       console.log("e", e);
+//   //     });
+//   // }
 
 
 
-  // Retorno
-  return (
-    <div className="App">
-      <h3>Cadastro de produtos: </h3>
-      {/* <p>{JSON.stringify(ssds)}</p> */}
-      <p>{JSON.stringify(objetoSsd)}</p>
+// const cadastrar = () => {
+//   fetch('http://localhost:9090/ssd', {
+//       method:'post',
+//       body:JSON.stringify(objetoSsd),
+//       headers:{
+//         'Content-type':'application/json',
+//         'Accept':'application/json'
+//       }
+//     })
+//     .then(retorno => retorno.json())
+//     .then(retorno_convertido => {
+      
+//       if(retorno_convertido.mensagem !== undefined){
+//         alert(retorno_convertido.mensagem);
+//       }else{
+//         setSsds([...ssds, retorno_convertido]);
+//         alert('Produto cadastrado com sucesso!');
+//         // limparFormulario();
+//       }
+      
+//     })
+//   }
 
-      <Formulario botao={btnCadastrar} eventoTeste={aoDiditar} obj={objetoSsd}  cadastrar={cadastrar} />
 
-      <List vetor={ssds} />
-      {/* <h3>Produtos cadastrados</h3> */}
-    </div>
-  );
-}
+//   // Retorno
+//   return (
+//     <div className="App">
+//       <h3>Cadastro de produtos: </h3>
+//       <p>{JSON.stringify(ssds)}</p>
+//       <p>{JSON.stringify(objetoSsd)}</p>
+
+//       <Formulario
+//         botao={btnCadastrar}
+//         eventoTeste={aoDiditar}
+//         cadastrar={cadastrar}
+        
+//       />
+
+//       <List vetor={ssds} />
+//       {/* <h3>Produtos cadastrados</h3> */}
+//     </div>
+//   );
+// }
 export default App;
