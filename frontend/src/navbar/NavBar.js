00fbc3671c31ab5010ce7e045c4450d6f1098d8c
arlindo <dinho-16@hotmail.com>
2023-06-25 15:20:12 -0300

    CPU CONTROLLER: LISTAR FUNCIONANDO. NAVBAR OK.

--- frontend/src/navbar/NavBar.js
+++ frontend/src/navbar/NavBar.js
@@ -1,36 +1,36 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 
-const NavBar = () =>{
-return(
+const NavBar = () => {
+  return (
 
-<nav class="nav-pills nav-fill">
-<button type="button" class="btn btn-primary">
-  <a class="nav-item nav-link">
-    <Link to="/home">Início</Link>
-  </a></button>
-<button type="button" class="btn btn-primary">
-  <a class="nav-item nav-link">
-    <Link to="/ssdlist">Listar SSDs</Link>
-  </a> </button>
-  <button type="button" class="btn btn-primary">
-  <a class="nav-item nav-link">
-    <Link to="/cpulist">Listar Cpus</Link>
-  </a> </button>
-  <button type="button" class="btn btn-primary">
-  <a class="nav-item nav-link">
-    <Link to="/clientList">Listar Clientes</Link>
-  </a> </button>
-<button type="button" class="btn btn-primary">
-  <a class="nav-item nav-link">
-    <Link to="//">Pesquisar produto</Link>
-  </a> </button>
-<button type="button" class="btn btn-primary">
-  <a class="nav-item nav-link">
-    <Link to="/dashboard">Dashboard</Link>
-  </a> </button>
-</nav>
+    <nav class="nav-pills nav-fill">
+      <button type="button" class="btn btn-primary">
+        <a class="nav-item nav-link">
+          <Link to="/home">Início</Link>
+        </a></button>
+      <button type="button" class="btn btn-primary">
+        <a class="nav-item nav-link">
+          <Link to="/ssdlist">Listar SSDs</Link>
+        </a> </button>
+      <button type="button" class="btn btn-primary">
+        <a class="nav-item nav-link">
+          <Link to="/cpulist">Listar Cpus</Link>
+        </a> </button>
+      <button type="button" class="btn btn-primary">
+        <a class="nav-item nav-link">
+          <Link to="/clientList">Listar Clientes</Link>
+        </a> </button>
+      <button type="button" class="btn btn-primary">
+        <a class="nav-item nav-link">
+          <Link to="//">Pesquisar produto</Link>
+        </a> </button>
+      <button type="button" class="btn btn-primary">
+        <a class="nav-item nav-link">
+          <Link to="/dashboard">Dashboard</Link>
+        </a> </button>
+    </nav>
 
-)
+  )
 }
 export default NavBar;
