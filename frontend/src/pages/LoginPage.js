e4ba7b70e4af6696a0add5e0eb5b765948a2d2b6
arlindo <dinho-16@hotmail.com>
2024-05-22 13:57:47 -0300

    correçao da navegação depois do login

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -4,10 +4,11 @@ import { authenticate, authFailure, authSuccess } from '../redux/authActions';
 import './loginpage.css';
 import { userLogin } from '../api/authenticationService';
 import { Alert, Spinner } from 'react-bootstrap';
-import NewUser
-    from './dashboard/NewUser';
-const LoginPage = ({ loading, error, ...props }) => {
+import NewUser from './dashboard/NewUser';
+import { useNavigate } from 'react-router-dom';
 
+const LoginPage = ({ loading, error, ...props }) => {
+    const navigate = useNavigate();
 
     const [values, setValues] = useState({
         userName: '',
@@ -19,47 +20,29 @@ const LoginPage = ({ loading, error, ...props }) => {
         props.authenticate();
 
         userLogin(values).then((response) => {
-
             console.log("response", response);
             if (response.status === 200) {
                 props.setUser(response.data);
-                props.history.push('/home');
-            }
-            if(response.status===404){
-                props.setUser(response.data);
-                props.authFailure('Usuário não cadastrado.')
-            }
-            else {
+                navigate('/home');
+            } else if (response.status === 404) {
+                props.authFailure('Usuário não cadastrado.');
+            } else {
                 props.loginFailure('Algo saiu errado. Tente novamente.');
             }
-
-
         }).catch((err) => {
-
             if (err && err.response) {
-
                 switch (err.response.status) {
                     case 401:
                         console.log("401 status");
-                        props.loginFailure("Authentication Failed.Bad Credentials");
+                        props.loginFailure("Authentication Failed. Bad Credentials");
                         break;
                     default:
                         props.loginFailure('Algo saiu errado. Tente novamente.');
-
                 }
-
-            }
-            else {
+            } else {
                 props.loginFailure('Algo saiu errado. Tente novamente.');
             }
-
-
-
-
         });
-        //console.log("Loading again",loading);
-
-
     }
 
     const handleChange = (e) => {
@@ -78,44 +61,49 @@ const LoginPage = ({ loading, error, ...props }) => {
                 <NewUser />
             </div>
 
-
             <section className="h-100">
                 <div className="container h-100">
-
                     <div className="row justify-content-md-center h-100">
                         <div className="card-wrapper">
-
                             <div className="card fat">
                                 <div className="card-body">
                                     <h4 className="card-title">Login</h4>
-
                                     <form className="my-login-validation" onSubmit={handleSubmit} noValidate={false}>
                                         <div className="form-group">
-                                            <label htmlFor="email">Usuário</label>
-                                            <input id="username" type="text" className="form-control"
-                                                minLength={5} value={values.userName} onChange={handleChange}
-                                                name="userName" required />
+                                            <label htmlFor="username">Usuário</label>
+                                            <input
+                                                id="username"
+                                                type="text"
+                                                className="form-control"
+                                                minLength={5}
+                                                value={values.userName}
+                                                onChange={handleChange}
+                                                name="userName"
+                                                required
+                                            />
                                             <div className="invalid-feedback">
                                                 UserId is invalid
                                             </div>
                                         </div>
 
                                         <div className="form-group">
-                                            <br></br> <br></br>
+                                            <br />
                                             <label htmlFor="password">Senha</label>
-                                            <input id="password" type="password" className="form-control" minLength={8} value={values.password} onChange={handleChange} name="password" required />
+                                            <input
+                                                id="password"
+                                                type="password"
+                                                className="form-control"
+                                                minLength={8}
+                                                value={values.password}
+                                                onChange={handleChange}
+                                                name="password"
+                                                required
+                                            />
                                             <div className="invalid-feedback">
                                                 Password is required
                                             </div>
                                         </div>
-
-                                        {/* <div className="form-group"> */}
-                                            {/* <div className="custom-control custom-checkbox"> */}
-                                                {/* <input type="checkbox" className="custom-control-input" id="customCheck1" /> */}
-                                                {/* <label className="custom-control-label" htmlFor="customCheck1">Lembrar login</label> */}
-                                            {/* </div> */}
-                                        {/* </div> */}
-                                        <br></br>
+                                        <br />
 
                                         <div className="form-group m-0">
                                             <button type="submit" className="btn btn-primary">
@@ -129,30 +117,21 @@ const LoginPage = ({ loading, error, ...props }) => {
                                                         aria-hidden="true"
                                                     />
                                                 )}
-                                                {/* <ClipLoader
-                                        //css={override}
-                                        size={20}
-                                        color={"#123abc"}
-                                        loading={loading}
-                                        /> */}
                                             </button>
                                         </div>
                                     </form>
-                                    <br></br>
+                                    <br />
                                     <div className="forgot">
                                         <a href="forgot.html" className="float-right">
                                             Forgot Password?
                                         </a>
-                                        <br></br>
+                                        <br />
                                     </div>
                                     {error &&
                                         <Alert style={{ marginTop: '20px' }} variant="danger">
                                             {error}
                                         </Alert>
-
                                     }
-
-
                                 </div>
                             </div>
                         </div>
@@ -160,29 +139,23 @@ const LoginPage = ({ loading, error, ...props }) => {
                 </div>
             </section>
         </div>
-    )
-
-
-
+    );
 }
 
 const mapStateToProps = ({ auth }) => {
-    console.log("state ", auth)
+    console.log("state ", auth);
     return {
         loading: auth.loading,
         error: auth.error
-    }
+    };
 }
 
-
 const mapDispatchToProps = (dispatch) => {
-
     return {
         authenticate: () => dispatch(authenticate()),
         setUser: (data) => dispatch(authSuccess(data)),
         loginFailure: (message) => dispatch(authFailure(message))
-    }
+    };
 }
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
--- frontend/src/pages/dashboard/dashboard.js
+++ frontend/src/pages/dashboard/dashboard.js
@@ -3,7 +3,7 @@ import { Button, Container } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { fetchUserData } from '../../api/authenticationService';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch,useNavigate } from "react-router-dom";
 import './dashboard.css';
 import NavBar from '../../navbar/NavBar';
 const MainWrapper = styled.div`
@@ -15,20 +15,21 @@ export const Dashboard = (props) => {
   const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState({});
+  const navigate = useNavigate();
 
   React.useEffect(() => {
     fetchUserData().then((response) => {
       setData(response.data);
     }).catch((e) => {
       localStorage.clear();
-      props.history.push('/');
+      navigate('/');
     })
   }, [])
 
   const logOut = () => {
 
     localStorage.clear();
-    props.history.push('/');
+    navigate('/');
 
   }
 
