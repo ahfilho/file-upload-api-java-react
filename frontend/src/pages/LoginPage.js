a22c45681664b8d4bc52d5c83753619b9c577884
arlindo <dinho-16@hotmail.com>
2024-08-02 12:45:58 -0300

    mudanças: login, navbar

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -91,11 +91,17 @@ const LoginPage = ({ loading, error, ...props }) => {
                     </button>
                     <button type="submit" className="btn px-">
                         <Link to="/dashboard">Dashboard</Link>
-                    </button></div>
+                    </button>
+                </div>
 
                 <form className="my-login-validation" onSubmit={handleSubmit} noValidate={false}>
-                    <div className="algumacoisa">Login</div>
-                    <div class="input-flex flex-column align-items-center">
+                    <div className="algumacoisa">
+                        <svg data-slot="icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path clip-rule="evenodd" fill-rule="evenodd" d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0Zm-5.5-2.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0ZM10 12a5.99 5.99 0 0 0-4.793 2.39A6.483 6.483 0 0 0 10 16.5a6.483 6.483 0 0 0 4.793-2.11A5.99 5.99 0 0 0 10 12Z"></path>
+                        </svg>
+                    </div>
+
+                    <div class=" input-flex flex-column align-items-center">
                         <div className="user">
                             Usuário
                         </div>
